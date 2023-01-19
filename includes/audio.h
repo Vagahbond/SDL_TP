4b99ecbfda4bc1c7bfbed5a610e65ba023ff6a92
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-01-19 16:41:11 +0100

    audio headers

---
+++ includes/audio.h
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+
+typedef struct {
+    SDL_AudioSpec spec;
+    Uint32 length;
+    Uint8 *buffer;
+    SDL_AudioDeviceID device_id;
+} AudioHandle;
+
+AudioHandle *create_audio_handle(SDL_RWops *file);
+
+void destroy_audio_handle(AudioHandle *handle);
